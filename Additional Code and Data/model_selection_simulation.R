1f99377ebee35e8488c941f7d17c44623aa47bb9
Neilblund <neilblund@gmail.com>
2025-01-13 10:12:29 -0500

    model selection simulation

--- Additional Code and Data/model_selection_simulation.R
+++ Additional Code and Data/model_selection_simulation.R
@@ -1,35 +1,70 @@
-# model selection function: performs stepwise selection based on the AIC (default)
-# or the BIC. The actual data generating model is Y ~ X1
-modelSelection<-function(N=100,bic =FALSE){
-  if(bic ==TRUE){
-    k<-log(N)
-  }else{
-    k<-2
+
+modelSelection<-function(reps= 100, N=100,bic =FALSE){
+  # model selection function: performs stepwise selection based on the AIC (default)
+  # or the BIC. The actual data generating model is Y ~ X1 * 2  + e
+  formulas<-character(reps)
+  pvalues<-matrix(ncol=5, nrow=reps)
+  colnames(pvalues) <- c("X1", "X2", "X3", "X4", "X5")
+  for(i in 1:reps){
+    if(bic ==TRUE){
+      k<-log(N)
+    }else{
+      k<-2
+    }
+    X<-replicate(5, rnorm(N))
+    Y<-1 + X[,1] * 2 + rnorm(N, 0, 1) # actual model
+    data<-data.frame(Y, X)
+    model<-lm(Y ~. , data=data)
+    
+    best<-step(model, 
+               direction = "both", 
+               scope = formula(~ .),
+               k = k,
+               trace=FALSE
+    )
+    f<-deparse(formula(best)) # return selected formula as a string
+    p <-coef(summary(best))   
+    # get p-values for selected model:
+    pvalues[i, ] <-    p[match( colnames(pvalues), rownames(p)),][,4]
+    formulas[i] <- f
+    
   }
-  X<-replicate(5, rnorm(N))
-  Y<-1 + X[,1] * 2 + rnorm(N) # actual model
-  data<-data.frame(Y, X)
-  model<-lm(Y ~. , data=data)
   
-  best<-step(model, 
-       direction = "both", 
-       scope = formula(~ .),
-       k = k,
-       trace=FALSE
-       )
-  f<-deparse(formula(best)) # return selected formula as a string
-  return(f)
+  data<-data.frame(formulas, pvalues)
+  
+  return(data)
 }
 
 set.seed(100)
 
-aic_reps<-replicate(100, modelSelection())
+# Model selection using the AIC: 
+aic_reps<-modelSelection(reps=500)  # correct model chosen ~ 50% of the time
+aic_reps$formulas|>
+  table()|>
+  sort(decreasing=TRUE)|>
+  prop.table()
 
 
-bic_reps<-replicate(100, modelSelection(bic=TRUE))
+
+rowSums(aic_reps[,3:ncol(aic_reps)]<.05, na.rm=T) |> 
+  table()|>
+  prop.table()|>
+  sort()|>
+  cumsum()              # 20% of models have at least one spurious correlation
 
 
-sort(table(aic_reps), decreasing=TRUE) # chooses the wrong model ~ 50% of the time
+# Model selection using the BIC
 
-sort(table(bic_reps), decreasing=TRUE) # chooses the wrong model ~ 10% of the time
+bic_reps<-modelSelection(reps=500, bic=TRUE) 
+bic_reps$formulas|>   # correct model chosen ~ 80%
+  table()|>
+  sort(decreasing=TRUE)|>
+  prop.table()
+
+
+rowSums(bic_reps[,3:ncol(bic_reps)]<.05, na.rm=T)|> 
+  table()|>
+  prop.table()|>
+  sort()|>
+  cumsum()            # ~14% of models have a spurious correlation
 
